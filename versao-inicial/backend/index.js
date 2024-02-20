e952b7b78a7de0356ba6b5f8f81d1d251da01d2b
rafael <silva.rafa@hotmail.com>
2024-02-20 00:37:27 -0300

    teste commit

---
+++ versao-inicial/backend/index.js
@@ -0,0 +1,4 @@
+const app = require('express')()                  //importando o express
+app.listen(3000, () => {                         //porta que iremos usar
+    console.log('Backend executable...')         //msg que aparecera no terminal ao rodarmos o npm start
+})
